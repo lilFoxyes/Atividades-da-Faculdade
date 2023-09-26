fffec88b0f23fd1f3ef48c5e74d1ef942a06f90c
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-09-26 17:57:31 -0300

    Add files via upload

--- 46.py
+++ 46.py
@@ -1,32 +1,42 @@
 dados = []
+altura_mulheres = []
+peso_homens = []
 
-while True:
-    nome, idade = input("Digite o seu nome e a sua idade: ").split()
+for i in range(5):
+    nome, peso, altura, sexo = input("Digite o seu nome, peso, altura e sexo: ").split()
+    peso = float(peso)
+    altura = float(altura)
+    dados.append((nome, peso, altura, sexo))
+
+for nome, peso, altura, sexo in dados:
+    if sexo == "feminino":
+        altura_mulheres.append(altura)
     
-    if nome == "Fim":
-        break
+    if sexo == "masculino":
+        peso_homens.append(peso)
+
+media_altura_mulheres = sum(altura_mulheres) / len(altura_mulheres)
+media_peso_homens = sum(peso_homens) / len(peso_homens)
+
+print("A média da altura feminina é {:.2f}".format(media_altura_mulheres))
+print("A média do peso masculino é {:.2f}kg".format(media_peso_homens))
+
+for nome, peso, altura, sexo in dados:
+    if sexo == "feminino" and altura > media_altura_mulheres:
+        print("{} tem {:.2f} e é mais alta que a média das mulheres.".format(nome, altura))
+
+for nome, peso, altura, sexo in dados:
+    if sexo == "feminino" and altura == max(altura for nome, _, altura, _ in dados):
+        print("A mais alta é a {} com {:.2f} de altura".format(nome, altura))
+
+for nome, peso, altura, sexo in dados:
+    if sexo == "masculino" and peso < media_peso_homens:
+        print("{} está abaixo da média de peso dos homens e pesa {:.2f}kg".format(nome, peso))
+
+for nome, peso, _, _ in dados:
+    if peso == max(peso for _, peso, _, _ in dados):
+        print("{} é o mais pesado e pesa {:.2f}kg".format(nome, peso))
+
+
+        
     
-    else:
-        idade = int(idade)
-        dados.append((nome, idade))
-
-
-if not dados:
-    print("Não coletamos dado algum")
-
-else :
-    idades = [idade for nome, idade in dados]
-    menor_idade = min(idades)
-    maior_idade = max(idades)
-    mais_novo = [(nome, idade) for nome, idade in dados if idade == menor_idade]
-    mais_velho = [(nome, idade) for nome, idade in dados if idade == maior_idade]
-    
-    for nome, idade in mais_novo:
-        print("O mais novo(a) dessa escola é o(a) {} e ele(a) tem {} anos".format(nome, idade))
-
-    for nome, idade in mais_velho:
-        print("O mais velho(a) dessa escola é o(a) {} e ele(a) tem {} anos".format(nome, idade))
-
-
-
-
